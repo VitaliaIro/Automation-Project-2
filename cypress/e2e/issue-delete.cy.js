b81041ec0e0402b10c1aeeb6332738b8636e379a
VitaliaIro <vitalia.iro@gmail.com>
2023-12-13 22:53:26 +0200

    Assignment #3 13.12.2023

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -1,3 +1,5 @@
+
+
 describe('Issue details deleting', () => {
 
     const issueTitle = 'This is an issue of type: Task.';
@@ -10,18 +12,37 @@ describe('Issue details deleting', () => {
       cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
         cy.visit(url + '/board');
         cy.contains('This is an issue of type: Task.').click();
-      });   
+      }); 
+      // Assert the visibility of the issue detail view modal  
+      cy.get(issueDetailViewModal).should('exist')
 });
 
-    it('Should delete issue successfully', () => {
-        
-        cy.get(iconTrash).click();
-        
+    it('Issue Deletion', () => {
+        // Delete the issue by clicking the delete button and confirming the deletion
+        cy.get(iconTrash).click();      
         cy.contains('button', 'Delete issue')
                 .click()
                 .should('not.exist');
+
+        // Assert that the deletion confirmation dialogue is not visible
         cy.contains(modalConfirm).should('not.exist');
+        // Assert that the issue is deleted and no longer displayed on the Jira board.
         cy.contains(issueTitle).should('not.exist');
+    });
+
+    it('Issue Deletion Cancellation', () => {
+      // Click the Delete Issue button
+      cy.get(iconTrash).click(); 
+      // Cancel the deletion in the confirmation pop-up
+      cy.contains('button', 'Cancel').click()
+
+      // Assert that the deletion confirmation dialogue is not visible
+      cy.contains(modalConfirm).should('not.exist');
+
+      // Assert that the issue is not deleted and is still displayed on the Jira board
+      cy.get('[data-testid="icon:close"]').eq(0).click();
+      cy.reload();
+      cy.contains(issueTitle).should('exist');
     });
       
 });
